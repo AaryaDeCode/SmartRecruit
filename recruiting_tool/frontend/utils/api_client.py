c0d9ce4813ff5d077fc597a7f527d6953659e34c
AaryaDeCode <166588747+AaryaDeCode@users.noreply.github.com>
2025-11-18 14:03:32 +0530

    Change API base URL to production endpoint

--- recruiting_tool/frontend/utils/api_client.py
+++ recruiting_tool/frontend/utils/api_client.py
@@ -1,11 +1,10 @@
 import requests
 
 class APIClient:
-    def __init__(self, base_url: str = "http://localhost:8000"):
+    def __init__(self, base_url: str = "https://smartrecruit-m4zo.onrender.com"):
         self.base_url = base_url
 
     def upload_resume(self, file, jd_text):
-        self.base_url = "https://smartrecruit-m4zo.onrender.com"
         url = f"{self.base_url}/api/resumes/upload-resume/"
         files = {"file": (file.name, file, "application/pdf")}
         data = {"job_description": jd_text}
@@ -18,4 +17,3 @@ class APIClient:
             print("Status Code:", response.status_code)
             print("Response Text:", response.text)
             return {"error": "Could not parse response from server"}
-
